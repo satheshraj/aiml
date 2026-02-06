5ebf36c021b8d0441106d4b1aa4b646c250e55c9
satheshraj <satheshrajsde@gmail.com>
2026-02-07 00:07:39 +0530

    Langchain practices

--- learning/langchain/langchain1.py
+++ learning/langchain/langchain1.py
@@ -12,6 +12,10 @@ else:
     print("API key not found")
 
 from langchain_openai import ChatOpenAI
-llm = ChatOpenAI(temperature=0, model="gpt-4-turbo", max_tokens=1000,api_key="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")
+llm = ChatOpenAI(temperature=0, model="gpt-4-turbo", max_tokens=1000,api_key=api_key)
 
-llm.invoke("Hello, how are you?")
+output=llm.invoke("Hello, how are you?")
+
+print(output)
+
+
