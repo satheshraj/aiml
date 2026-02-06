d416aae79848e9c943498e739734c01b2555659a
satheshraj <satheshrajsde@gmail.com>
2026-02-07 00:05:23 +0530

    Langchain practices

--- learning/langchain/langchain.py
+++ learning/langchain/langchain.py
@@ -9,7 +9,7 @@ if api_key is not None:
     # Use the key, e.g.,
     # response = requests.get(f"https://api.example.com/data?key={api_key}")
 else:
-    print("API key not found.")
+    print("API key not found")
 
 from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(temperature=0, model="gpt-4-turbo", max_tokens=1000,api_key="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")
