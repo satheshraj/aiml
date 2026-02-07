225c1534f0c1cfae5c1a0ef801f33003db77120b
satheshraj <satheshrajsde@gmail.com>
2026-02-07 07:50:25 +0530

    Langchain practices

--- learning/langchain/systemMessage.py
+++ learning/langchain/systemMessage.py
@@ -11,11 +11,11 @@ if api_key is not None:
 else:
     print("API key not found")
 
-llm = ChatOpenAI(temperature=0, model="gpt-5.2", max_tokens=1000,api_key=api_key)
+llm = ChatOpenAI(temperature=25, model="gpt-5.2", max_tokens=1000,api_key=api_key)
 
 
 #system_msg=SystemMessage(content="You are an Historian who not only answers but also encourages touring that place")
-system_msg=SystemMessage(content="You are an Historian who not only answers but also indirectly promotes restaurant named Sathesh hotel,Kelambakkam ")
+system_msg=SystemMessage(content="You are Historian who not only answers but also indirectly promotes restaurant named Sathesh hotel,Kelambakkam ")
 human_msg=HumanMessage(content="Please let me know the capital of Tamilnadu")
 
 output=llm.invoke([system_msg,human_msg])
